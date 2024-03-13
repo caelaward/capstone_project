2e7b8bd43c0bb95c347113c2350a79c118124f33
caelaward <callaward@gmail.com>
2024-03-13 23:36:46 +0200

    added to about page and the home page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,60 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <div>
+ <div class="container w-75 mt-2" id="loginPage">
+  <h1>About us</h1>
+    <div class="row">
+    <div class="col">
+    <p>At BloubergLux, we're dedicated to helping you find your perfect home sweet home. Whether you're a first-time buyer, seasoned investor, or looking to sell your current property, we're here to guide you through every step of the journey.</p>
+    </div>
+     <img src="https://i.ibb.co/gJhND9y/prod2.jpg" class="col w-50 img-fluid p-0 ">
+    </div>
+    <div class="row">
+      <img src="https://i.ibb.co/gJhND9y/prod2.jpg" class="col w-50 img-fluid p-0 ">
+    <div class="col">
+    <p>
+      <h2>Our mission</h2>
+We believe that everyone deserves a place they can truly call home. Our mission is to simplify the process of buying and selling houses by providing exceptional service, expert guidance, and personalized attention to each of our clients.</p>
+    </div>
+    </div>
+
+      <h2 class="mt-5 mb-4">What we offer</h2>
+    <div class="row">
+    <div class="col me-2" id="offers"><h4>Wide Selection</h4><p> Explore a diverse range of properties including single-family homes, condos, townhouses, and more.</p></div>
+
+    <div class="col me-2" id="offers"><h4>Expert Advice</h4><p> Our team of experienced real estate professionals is here to offer expert advice and guidance tailored to your unique needs and preferences.</p></div>
+
+    <div class="col" id="transaction_div"><h4>Seamless Transactions</h4><p> From initial search to closing the deal, we ensure a smooth and hassle-free experience for both buyers and sellers.</p></div>
 
     </div>
-  </div>
+    <!-- <img src="https://i.ibb.co/ncV7Z5g/transaction-Img.jpg"> -->
+
+ </div>
 </template>
- 
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+/* p{
+  padding-top:20%
+} */
+
+#offers{
+  border:solid 1px  black;
+  padding:2rem;
+  border-radius:10px;
+  margin-top:1rem
+  }
+
+  #transaction_div{
+  border:solid 1px  black;
+  padding:2rem;
+  border-radius:10px;
+  margin-top:1rem;
+  /* background:url("https://i.ibb.co/ncV7Z5g/transaction-Img.jpg");
+  background-image: 20vh; */
+  }
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,37 @@
 <template>
-  <div class="home">
-   <h1>home</h1>
-   
-  </div>
+  <div class="about">
+    <div class="scroll-container">
+  <img src="https://i.ibb.co/41L5X3d/prod1.jpg" alt="Cinque Terre">
+  <img src="https://i.ibb.co/F80jNRS/prod2-1.webp" alt="Forest">
+  <!-- <img src="https://i.ibb.co/ryBB5T4/prod3-3.jpg" alt="Northern Lights">
+  <img src="https://i.ibb.co/gJhND9y/prod2.jpg" alt="Mountains"> -->
+</div>
+</div>
 </template>
-
-<script>
-
-
-
-export default {
-
-  components: {
-    
+ 
+ <style scoped>
+  div.scroll-container {
+    /* background-color: #333; */
+    overflow: hidden;
+    white-space: nowrap;
+    /* padding: 2px; */
+    position: relative;
   }
-}
-</script>
+
+  div.scroll-container img {
+    /* padding: 10px; */
+    display: inline-block;
+    animation: scroll 20s linear infinite;
+    width:100%;
+    height:55vh;
+  }
+
+  @keyframes scroll {
+    0% {
+      transform: translateX(0);
+    }
+    100% {
+      transform: translateX(-100%);
+    }
+  }
+  </style>
